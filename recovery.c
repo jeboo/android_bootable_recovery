61bd89075f7373418804d52c5cdd97e764830264
Your Name <jeb00o@yahoo.com>
2014-05-16 11:39:09 -0400

    version 2.4

--- recovery.c
+++ recovery.c
@@ -521,7 +521,7 @@ copy_sideloaded_package(const char* original_path) {
 
 static const char**
 prepend_title(const char** headers) {
-    const char* title[] = { "Jeboo Recovery v2.3",
+    const char* title[] = { "Jeboo Recovery v2.4",
                       "",
                       NULL };
 
